bc18c183ad63c12d1e8c2b328eae2610c19fc96d
unknown <Babz@Babz-Laptop.(none)>
2012-01-04 18:28:23 +0100

    organized imports

--- dslab2/src/remote/INotifyClientCallback.java
+++
@@ -1,9 +0,0 @@
-package remote;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface INotifyClientCallback extends Remote {
-	
-	public void sendNotification(int taskId) throws RemoteException;
-}
